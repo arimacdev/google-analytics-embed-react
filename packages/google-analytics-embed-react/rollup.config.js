c4af34b67e556dee762ac573520f1e12766b532d
WhizSid <whizsid@aol.com>
2023-01-20 01:58:11 +0530

    Fixed class properties issue

--- packages/google-analytics-embed-react/rollup.config.js
+++ packages/google-analytics-embed-react/rollup.config.js
@@ -1,6 +1,7 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 
 const packageJson = require('./package.json');
@@ -20,11 +21,18 @@ export default {
     }
   ],
   plugins: [
+    typescript(),
+      babel({ 
+          babelHelpers: 'bundled',
+          exclude: 'node_modules/**',
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          plugins: [],
+          presets: ["@babel/preset-react", "@babel/preset-env"]
+      }),
     peerDepsExternal(),
     resolve({
       browser: true
     }),
-    commonjs(),
-    typescript()
+    commonjs()
   ]
 };
